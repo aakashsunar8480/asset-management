6a8efbf6cebb21801140f55d41bb85a5845be635
aakashsunar8480”git config --global user.name aakashsunar8480” <aakash.sunar@data-axle.com>
2023-08-06 22:40:40 +0530

    Asset model relations added

--- backend/project/asset_management/asset_management_app/models/Asset/assets.py
+++ backend/project/asset_management/asset_management_app/models/Asset/assets.py
@@ -1,15 +1,18 @@
 from django.db import models
 from ..Users import Users
+from .subcategory import SubCategory
+from .vendors import Vendors
+from .warranty import Warranty
 from asset_management_app.enums import AssetStatusEnum
 
 
 class Assets(models.Model):
 
     asset_id = models.CharField(max_length=200, unique=True)
-    subcategory = models.CharField()
-    warranty = models.CharField()
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    warranty = models.ForeignKey(Warranty,on_delete=models.CASCADE)
     user = models.ForeignKey(Users,on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=[(choice.value, choice.name) for choice in AssetStatusEnum])
     primary_owner = models.CharField()
     purchase_date = models.DateField()
-    vendor = models.CharField()
+    vendor = models.ForeignKey(Vendors,on_delete=models.CASCADE)
